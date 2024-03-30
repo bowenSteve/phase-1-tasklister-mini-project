12ac5ea8cb6bd237086da425b85a95007fb28494
Stephen Bowen <bowensteven17@gmail.com>
2024-03-30 11:07:28 +0300

    Complete tasklister mini project lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let myTask = document.getElementById('new-task-description')
+    addList((myTask.value));
+    e.target.reset()
+  })
 });
+function addList(task){
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click',deleteFunction)
+  btn.textContent=(' X ');
+  li.textContent= ` ${task} `;
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+  
+
+}
+function deleteFunction(e){
+    e.target.parentNode.remove()
+
+}
